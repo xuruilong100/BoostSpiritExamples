00c530c2efd5d20b2ceaed3025e3ad36ad357c02
xuruilong100 <xuruilong100@hotmail.com>
2026-06-07 12:22:56 +0800

    Add Boost license headers and build tweaks

--- Qi/Calc6/main.cpp
+++ Qi/Calc6/main.cpp
@@ -1,4 +1,11 @@
-﻿#include "ast.hpp"
+﻿/*=============================================================================
+    Copyright (c) 2001-2011 Joel de Guzman
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+
+#include "ast.hpp"
 #include "calculator.hpp"
 #include "compiler.hpp"
 #include "error_handler.hpp"
--- Qi/Calc8/vm.cpp
+++ Qi/Calc8/vm.cpp
@@ -4,8 +4,8 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
+
 #include "vm.hpp"
-#include <boost/assert.hpp>
 
 namespace client {
 double vmachine::execute(std::vector<double> const& code,
